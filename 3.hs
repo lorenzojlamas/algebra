e36e4f569b032c7a2e892be9d0606df46887decf
lorenzojlamas <lorenzojlamas@gmail.com>
2016-05-10 11:53:49 -0300

    NO me salio el ultimo ejercicio

--- 3.hs
+++ 3.hs
@@ -40,3 +40,25 @@ dobleFact n | n  == 0 = 1
 
 noTermina n | n >= 0 = True 
             | n < 0 = noTermina(-1)
+
+mul3 :: Integer -> Bool
+mul3 n | n == 1 = False
+       | n == 2 = False
+       | n == 0 = True
+       | n < 0 = mul3 (-n)
+       | n > 0 = mul3 (n-3)
+       | otherwise = error "Mal ahi"
+
+-- sumaImparsCuyoCuadSeaMenorQue :: Integer -> Integer
+-- sumaImparsCuyoCuadSeaMenorQue n | n < 0 = error "Son negativos papa"
+--                                | n > 0 = 
+--ultimoN :: (Integer,Integer) -> Integer
+--ultimoN n b | n > b^2 = ultimoN (n , b+1)
+--            | n == b^2 = b
+--            | n < b^2 = (b-1)
+
+--ultimoImpar :: Integer -> Integer
+--ultimoImpar n | mod (n,2) == 0 = n-1 
+--              | mod (n,2) /= 0 = n
+
+-- aaaaaaaaaaaaaaaaaahhhhhhhhhhhhhhhhhhhhhh no anda!!!!!!!!!!!
