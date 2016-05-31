1eae46fa82ec259757b4c94e2b63353d509dfafc
lorenzojlamas <lorenzojlamas@gmail.com>
2016-05-31 12:12:37 -0300

    Sin Terminar 6 y principio 7

--- 5.hs
+++ 5.hs
@@ -1,5 +1,22 @@
--- 18/03/16
+-- 16/05/16
 -- Lorenzo Jose Lamas
 -- Algebra I
 -- Labo
 --
+
+pertenece :: Integer -> [Integer] -> Bool
+
+pertenece a xs | xs == [] = False
+			   | a == head xs = True
+			   | a /= head xs = pertenece a (tail xs)
+
+
+hayRepetidos :: [Integer] -> Bool
+
+hayRepetidos xs | length xs < 2 = False
+				| head xs == head (tail xs) = True
+				| head xs /= head (tail xs) = hayRepetidos (tail xs)
+
+
+
+
